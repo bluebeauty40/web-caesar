b1e6ce3078c15ff9f65e3da50251f32110923d3f
Shaneen Lasley <bluebeauty40@gmail.com>
2019-06-17 13:29:43 -0500

    Finish web caesar

---
+++ caesar.py
@@ -0,0 +1,44 @@
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+   <head>
+       <style>
+           form {{
+               background-color: #eee;
+               padding: 20px;
+               margin: 0 auto;
+               width: 540px;
+               font: 16px sans-serif;
+               border-radius: 10px;
+           }}
+           textarea {{
+               margin: 10px 0;
+               width: 540px;
+               height: 120px;
+           }}
+       </style>
+   </head>
+   <body>
+   <form method='POST'>
+       <label>Rotate By:</label>
+       <input type="text" name="rot" value="0" />
+       <textarea name="text">{0}</textarea>
+       <input type="Submit" value="Submit Query" />
+    </form>
+   
+   </body>
+</html>
+"""
+           
+
+@app.route("/")
+def index():
+    return form.format('')
+
+app.run()
